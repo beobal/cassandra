c37c4d5ea4a66789d5eb1915ecf943219d44a33a
Sam Tunnicliffe <samt@apache.org>
2024-10-18 15:01:39 +0100

    Track inflight debounce futures and cancel them when shutting down

--- src/java/org/apache/cassandra/tcm/EpochAwareDebounce.java
+++ src/java/org/apache/cassandra/tcm/EpochAwareDebounce.java
@@ -21,9 +21,15 @@ package org.apache.cassandra.tcm;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.apache.cassandra.utils.Closeable;
 import org.apache.cassandra.utils.concurrent.Future;
 import org.apache.cassandra.utils.concurrent.ImmediateFuture;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.apache.cassandra.config.DatabaseDescriptor.getCmsAwaitTimeout;
+import static org.apache.cassandra.config.DatabaseDescriptor.getEpochAwareDebounceInFlightTrackerMaxSize;
 
 /**
  * When debouncing from a replica we know exactly which epoch we need, so to avoid retries we
@@ -36,6 +42,11 @@ public class EpochAwareDebounce implements Closeable
     public static final EpochAwareDebounce instance = new EpochAwareDebounce();
 
     private final AtomicReference<EpochAwareFuture> currentFuture = new AtomicReference<>();
+    private final Cache<Epoch, Future<ClusterMetadata>> inflight = Caffeine.newBuilder()
+                                                                           .maximumSize(getEpochAwareDebounceInFlightTrackerMaxSize())
+                                                                           .expireAfterWrite(getCmsAwaitTimeout().to(MILLISECONDS),
+                                                                                             MILLISECONDS)
+                                                                           .build();
 
     private EpochAwareDebounce()
     {
@@ -66,10 +77,16 @@ public class EpochAwareDebounce implements Closeable
 
             if (currentFuture.compareAndSet(running, SENTINEL))
             {
-                EpochAwareFuture promise = new EpochAwareFuture(epoch, fetchFunction.get());
+                Future<ClusterMetadata> cmFuture = fetchFunction.get();
+                cmFuture.addListener(() -> inflight.asMap().remove(epoch));
+                EpochAwareFuture promise = new EpochAwareFuture(epoch, cmFuture);
                 EpochAwareFuture current = currentFuture.compareAndExchange(SENTINEL, promise);
                 // we have to explicitly check here as close() unconditionally sets currentFuture
-                return current == CLOSED ? CLOSED.future : promise.future;
+                if (current == CLOSED)
+                    return CLOSED.future;
+
+                inflight.put(epoch, cmFuture);
+                return cmFuture;
             }
         }
     }
@@ -83,6 +100,13 @@ public class EpochAwareDebounce implements Closeable
         EpochAwareFuture future = currentFuture.getAndSet(CLOSED);
         if (future != null && future != SENTINEL && future != CLOSED)
             future.future.cancel(true);
+        for (Future<ClusterMetadata> toCancel : inflight.asMap().values())
+            toCancel.cancel(true);
+    }
+
+    public long inflightTrackerSize()
+    {
+        return inflight.estimatedSize();
     }
 
     private static class EpochAwareFuture
